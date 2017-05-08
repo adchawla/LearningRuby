e61bdaa63b1f5e57d541379c1fa495c1cd9a5c89
Amandeep Chawla <adchawla@gmail.com>
2017-05-08 10:15:29 +0530

    Using puts in place of print

--- 001.rb
+++ 001.rb
@@ -1,5 +1,5 @@
-print "Say something, please: "
+puts "Say something, please: "
 user_input = gets.chomp
-print "You said #{user_input}.\n"
-print "Backwards, it reads #{user_input.reverse}.\n"
-print "The message is #{user_input.length} characters long.\n"
+puts "You said #{user_input}.\n"
+puts "Backwards, it reads #{user_input.reverse}.\n"
+puts "The message is #{user_input.length} characters long.\n"
