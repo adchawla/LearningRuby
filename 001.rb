aab004ab4cb530e6e30874794e92bb5d5b0a7e21
Amandeep Chawla <adchawla@gmail.com>
2017-05-08 10:09:48 +0530

    My First Ruby Script

---
+++ 001.rb
@@ -0,0 +1,5 @@
+print "Say something, please: "
+user_input = gets.chomp
+print "You said #{user_input}.\n"
+print "Backwards, it reads #{user_input.reverse}.\n"
+print "The message is #{user_input.length} characters long.\n"
